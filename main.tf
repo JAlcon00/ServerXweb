3674707d05d6f617aca3414688cb9f5b3f002607
JAlcon00 <c.jesusalmanza@gmail.com>
2024-11-26 12:23:13 -0600

    Updating server config 4

--- main.tf
+++ main.tf
@@ -10,11 +10,10 @@ terraform {
     bucket                        = "devjesus2"
     key                           = "terraform.tfstate"
     region                        = "us-east-1"
-    skip_credentials_validation   = true
-    skip_metadata_api_check       = true
-    skip_requesting_account_id    = true
-    use_path_style                = true
-    disable_sha256                = true
+    skip_credentials_validation = true
+    skip_metadata_api_check    = true
+    skip_region_validation     = true
+    force_path_style           = true
   }
 }
 
@@ -23,7 +22,7 @@ provider "digitalocean" {
 }
 
 resource "digitalocean_project" "yisus" {
-  name        = "yisus-server"
+  name        = "yisus-server-${formatdate("YYYYMMDD", timestamp())}" 
   description = "Proyecto para desplegar el servidor backend"
   purpose     = "Web Application"
   environment = "Production"
