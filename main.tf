70c5730973559789864996ed64b7faa9323d8abc
JAlcon00 <c.jesusalmanza@gmail.com>
2024-11-26 12:25:27 -0600

    Updating server config 5

--- main.tf
+++ main.tf
@@ -10,10 +10,11 @@ terraform {
     bucket                        = "devjesus2"
     key                           = "terraform.tfstate"
     region                        = "us-east-1"
-    skip_credentials_validation = true
-    skip_metadata_api_check    = true
-    skip_region_validation     = true
-    force_path_style           = true
+    skip_credentials_validation   = true
+    skip_metadata_api_check       = true
+    skip_requesting_account_id    = true
+    use_path_style                = true
+    disable_sha256                = true
   }
 }
 
