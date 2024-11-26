8720c91044fc11457b3a5cccb2c8465cf035269a
JAlcon00 <c.jesusalmanza@gmail.com>
2024-11-26 12:09:21 -0600

    Updating server config 3

--- main.tf
+++ main.tf
@@ -5,18 +5,16 @@ terraform {
       version = "~> 2.0"
     }
   }
-
   backend "s3" {
-    endpoints = {
-      s3 = "https://nyc3.digitaloceanspaces.com"
-    }
-    bucket                      = "devjesus2"
-    key                         = "terraform.tfstate"
-    region                      = "us-east-1"
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
-    skip_requesting_account_id  = true
-    force_path_style           = true
+    endpoint                      = "https://nyc3.digitaloceanspaces.com"
+    bucket                        = "devjesus2"
+    key                           = "terraform.tfstate"
+    region                        = "us-east-1"
+    skip_credentials_validation   = true
+    skip_metadata_api_check       = true
+    skip_requesting_account_id    = true
+    use_path_style                = true
+    disable_sha256                = true
   }
 }
 
