cb22e24a9254989cfd25f3d2d84ccb811ba6c285
JAlcon00 <c.jesusalmanza@gmail.com>
2024-11-26 14:59:08 -0600

    Updating server config 18

--- main.tf
+++ main.tf
@@ -10,14 +10,14 @@ terraform {
     endpoints = {
       s3 = "https://nyc3.digitaloceanspaces.com"
     }
-    bucket                     = "devjesus2"
-    key                        = "terraform.tfstate"
-    region                     = "us-east-1"
+    bucket                      = "devjesus2"
+    key                         = "terraform.tfstate"
+    region                      = "us-east-1"
     skip_credentials_validation = true
-    skip_metadata_api_check    = true
-    skip_region_validation     = true
-    skip_requesting_account_id = true
-    use_path_style            = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    use_path_style             = true
   }
 }
 
